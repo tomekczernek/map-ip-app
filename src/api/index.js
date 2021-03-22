5b1f6ceded5c788f34b98afd6fa386d480858dbf
tomekczernek <tomekczernek@gmail.com>
2021-03-22 02:54:55 +0100

    Changes in api

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,9 @@ import axios from 'axios';
 
 function request(method, endpoint, data = null){
 
-    const REACT_APP_URL = 'http://api.ipstack.com/';
-    const REACT_APP_KEY = 'acfbf76f9cab2347b7e596611c25731e';
-
     return axios({
         method,
-        //url: `${process.env.REACT_APP_URL}${endpoint}${'?access_key='}${process.env.REACT_APP_KEY}`,
-        url: `${REACT_APP_URL}${endpoint}${'?access_key='}${REACT_APP_KEY}`,
+        url: `${process.env.REACT_APP_URL}${endpoint}${'?access_key='}${process.env.REACT_APP_KEY}`,
         data
     });
 }
